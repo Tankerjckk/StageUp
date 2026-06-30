c75a61d47401e832d5c23e047ec243510d0eb655
Jacek <tankerjckk@gmail.com>
2026-06-30 17:21:26 +0200

    Icons etc 1.1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,16 +37,15 @@ export const metadata: Metadata = {
 
   creator: "StageUp",
 
-  icons: {
-    icon: [
-      { url: "/favicon.ico" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-    ],
-    apple: "/apple-touch-icon.png",
-  },
-
-  manifest: "/site.webmanifest",
+icons: {
+  icon: [
+    { url: "/favicon.ico" },
+    { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+  ],
+  apple: "/apple-touch-icon.png",
+},
+manifest: "/site.webmanifest",
 
   openGraph: {
     type: "website",
